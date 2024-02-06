eca3cf35afac517345339c896f24782145ace52e
Barış Kahraman <baris-kahra@hotmail.com>
2024-02-06 20:35:42 +0300

    Dark modun local storage a kaydedilmesi işlemi yapıldı.

--- assets/js/themeSelector.js
+++ assets/js/themeSelector.js
@@ -3,14 +3,46 @@ const toggleDarkMode = document.querySelector('.toggle-fill');
 const sunIcon = document.querySelector('#sun');
 const moonIcon = document.querySelector('#moon');
 
-toggleDarkMode.addEventListener('click',function(){
-    if(body.classList.contains('dark-mode')){
+let saveTheme = JSON.parse(localStorage.getItem('theme')) || [];
+
+function saveThemeToLocalStorage() {
+    localStorage.setItem('theme', JSON.stringify(saveTheme));
+}
+
+toggleDarkMode.addEventListener('click', switchTheme);
+
+function switchTheme() {
+    saveTheme = [];
+    if (body.classList.contains('dark-mode')) {
         body.classList.remove('dark-mode');
         sunIcon.src = "assets/img/sun.svg";
         moonIcon.src = "assets/img/moon.svg";
-    }else{
+        saveTheme.push({
+            theme: 'light'
+        });
+        saveThemeToLocalStorage();
+    } else {
         body.classList.add('dark-mode');
         sunIcon.src = "assets/img/dark-mode-sun.svg";
         moonIcon.src = "assets/img/dark-mode-moon.svg";
+        saveTheme.push({
+            theme: 'dark'
+        });
+        saveThemeToLocalStorage();
     }
-})
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (saveTheme.length > 0) {
+        const currentTheme = saveTheme[0].theme;
+        if (currentTheme === 'light') {
+            body.classList.remove('dark-mode');
+            sunIcon.src = "assets/img/sun.svg";
+            moonIcon.src = "assets/img/moon.svg";
+        } else if (currentTheme === 'dark') {
+            body.classList.add('dark-mode');
+            sunIcon.src = "assets/img/dark-mode-sun.svg";
+            moonIcon.src = "assets/img/dark-mode-moon.svg";
+        }
+    }
+});
